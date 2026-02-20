2f9c6e76c900b9861bbab539012e7aee8b5946e5
VOL-7E5JPI13\Valerie Orozco <orozcolv@sheridancollege.ca>
2026-02-20 00:23:44 -0500

    Interactive Toy: I created the pixelart images for the cupcake's toppings, then added them to the scene and made them prefabs. Then, I added the UI components like the buttons and the sliders. After organizing the scene, I created the scrips and started modifying the first one that makes the toppings spawn and get them destroyed if pressed again.

---
+++ Assets/Scripts/Scripts for Toy/Appear.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class Appear : MonoBehaviour
+{
+    public GameObject toppingPrefab; //this one will be either the syrup, frosting or sprinkles prefab
+    private GameObject spawnedTopping; 
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AppearTopping ()
+    {
+       if (spawnedTopping == null)
+        {
+            spawnedTopping = Instantiate(toppingPrefab); //creates topping in the scene
+        }
+        else
+        {
+            Destroy(spawnedTopping); //destroy the topping from the scene
+            spawnedTopping = null;
+        }
+
+    }
+
+}
